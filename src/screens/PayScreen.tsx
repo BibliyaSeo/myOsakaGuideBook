ab49dad13953453fd2484009a611024adb82e006
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-12-05 18:04:57 +0900

    Feat: PayScreen

--- src/screens/PayScreen.tsx
+++ src/screens/PayScreen.tsx
@@ -1,7 +1,27 @@
 import {View, Text, SafeAreaView} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {api} from '../api';
+import {NOTION_PAY_DATABASE_ID} from '@env';
 
 export default function PayScreen() {
+  useEffect(() => {
+    const daySort = {
+      sorts: [
+        {
+          property: 'date',
+          direction: 'ascending',
+        },
+      ],
+    };
+
+    const getOsakaPay = async () => {
+      try {
+        const res = api.post(`/${NOTION_PAY_DATABASE_ID}/query`, daySort);
+        console.log(res);
+      } catch (error) {}
+    };
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>PayScreen</Text>
